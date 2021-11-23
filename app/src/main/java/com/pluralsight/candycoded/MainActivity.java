15c17ed68057c28dbacc54d5586652cfa91331f7
wilfred gicharu <wilfredgicharu254@gmail.com>
2021-11-23 16:11:44 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -73,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         });
   }
 
+
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     MenuInflater inflater = getMenuInflater();
@@ -82,6 +84,13 @@ public class MainActivity extends AppCompatActivity {
   // ***
   // TODO - Task 1 - Show Store Information Activity
   // ***
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item){
+    Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
+    startActivity(infoIntent);
+
+    return super.onOptionsItemSelected(item);
+  }
 
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
